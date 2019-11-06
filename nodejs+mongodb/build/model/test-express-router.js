8a5d68d8d1ea0c1a6e92b37e2177cadd9c0ecfd3
pengyw <lonelyblack2010@hotmail.com>
2019-11-07 02:34:40 +0800

    xx

--- nodejs+mongodb/build/model/test-express-router.js
+++ nodejs+mongodb/build/model/test-express-router.js
@@ -10,7 +10,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const url = __importStar(require("url"));
 const ejs = __importStar(require("ejs"));
 const fs = __importStar(require("fs"));
+const MongoClient = __importStar(require("mongodb"));
 var G = [];
+var dburl = 'mongodb://localhost:27017/';
 function app(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     var pathname = "";
@@ -36,6 +38,11 @@ app.prototype.get = function (path, callback) {
 };
 app.prototype.get('login', function (req, res) {
     ejs.renderFile('views/login.ejs', {}, function (err, data) {
+        res.end(data);
+    });
+});
+app.prototype.get('user', function (req, res) {
+    ejs.renderFile('views/user.ejs', {}, function (err, data) {
         res.end(data);
     });
 });
@@ -73,6 +80,100 @@ app.prototype.get('home', function (req, res) {
     var list = [1, 2, 3, 4];
     ejs.renderFile('views/index.ejs', { msg: data, l: list }, function (err, data) {
         res.end(data);
+    });
+});
+app.prototype.get('add', function (req, res) {
+    if (req.method == 'GET') {
+        console.log('get');
+        console.log(url.parse(req.url, true).query);
+    }
+    else if (req.method == 'POST') {
+        var postStr = '';
+        req.on('data', function (chunk) {
+            postStr += chunk;
+        });
+        req.on('end', function (err, chunk) {
+            var param = {};
+            postStr.split("&").forEach(element => {
+                var key = element.split("=")[0];
+                var value = element.split("=")[1];
+                param[key] = value;
+            });
+            MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+                if (err)
+                    throw err;
+                var dbo = db.db("test");
+                var myobj = { name: param['username'], age: param['age'], sex: param['sex'] };
+                dbo.collection("user").insertOne(myobj, function (err, result) {
+                    if (err)
+                        throw err;
+                    console.log("文档插入成功");
+                    res.end("<script>alert('文档插入成功'); history.back();</script>");
+                    // dbo.collection("user").find().toArray(function (err, result) {
+                    //     if (err) throw err;
+                    //     console.log(result);
+                    //     ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                    //         res.end(data);
+                    //     });
+                    //     db.close();
+                    // });
+                    db.close();
+                });
+            });
+            // res.end("<script>alert('创建成功');</script>"); history.back();
+        });
+    }
+});
+app.prototype.get('getuser', function (req, res) {
+    MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+        if (err)
+            throw err;
+        var dbo = db.db("test");
+        dbo.collection("user").find().toArray(function (err, result) {
+            if (err)
+                throw err;
+            console.log(result);
+            ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                res.end(data);
+            });
+            db.close();
+        });
+    });
+});
+app.prototype.get('del', function (req, res) {
+    var postStr = '';
+    req.on('data', function (chunk) {
+        postStr += chunk;
+    });
+    req.on('end', function (err, chunk) {
+        console.log(postStr);
+    });
+    res.end();
+    // MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+    //     if (err) throw err;
+    //     var dbo = db.db("test");
+    //     var whereStr = { "name": '菜鸟教程' };  // 查询条件
+    //     dbo.collection("site").deleteOne(whereStr, function (err, obj) {
+    //         if (err) throw err;
+    //         console.log("文档删除成功");
+    //         db.close();
+    //     });
+    // });
+});
+app.prototype.get('edit', function (req, res) {
+    MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+        if (err)
+            throw err;
+        var dbo = db.db("test");
+        dbo.collection("user").find().toArray(function (err, result) {
+            if (err)
+                throw err;
+            console.log(result);
+            ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                res.end(data);
+            });
+            db.close();
+        });
     });
 });
 // // 装饰器
--- nodejs+mongodb/model/test-express-router.ts
+++ nodejs+mongodb/model/test-express-router.ts
@@ -1,17 +1,20 @@
 import * as url from 'url';
 import * as ejs from 'ejs';
 import * as fs from 'fs';
+import * as MongoClient from 'mongodb'
 
 var G: any = [];
 
+var dburl = 'mongodb://localhost:27017/';
+
 export function app(req: any, res: any) {
 
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 
     var pathname: string = ""
     if (req.url != null) {
         var originalPath = url.parse(req.url).pathname;
-        if (originalPath != null) { 
+        if (originalPath != null) {
             pathname = originalPath.replace('/', '');
         }
     }
@@ -30,24 +33,30 @@ app.prototype.get = function (path: string, callback: Function) {
     G[path] = callback;
 }
 
-app.prototype.get('login', function(req: any, res: any) {
-    ejs.renderFile('views/login.ejs', {}, function(err, data) {
+app.prototype.get('login', function (req: any, res: any) {
+    ejs.renderFile('views/login.ejs', {}, function (err, data) {
         res.end(data);
     });
 });
 
-app.prototype.get('dologin', function(req: any, res: any) {
+app.prototype.get('user', function (req: any, res: any) {
+    ejs.renderFile('views/user.ejs', {}, function (err, data) {
+        res.end(data);
+    });
+});
+
+app.prototype.get('dologin', function (req: any, res: any) {
     if (req.method == 'GET') {
         console.log('get');
         console.log(url.parse(req.url, true).query);
     } else if (req.method == 'POST') {
         var postStr = '';
-        req.on('data', function(chunk: any) {
+        req.on('data', function (chunk: any) {
             postStr += chunk;
         });
-        req.on('end', function(err: any, chunk: any) {
+        req.on('end', function (err: any, chunk: any) {
             console.log(postStr);
-            fs.appendFile('login.txt', postStr + '\n', function(err: any) {
+            fs.appendFile('login.txt', postStr + '\n', function (err: any) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -59,18 +68,112 @@ app.prototype.get('dologin', function(req: any, res: any) {
     }
 });
 
-app.prototype.get('register', function(req: any, res: any) {
-    ejs.renderFile('views/register.ejs', {}, function(err, data) {
+app.prototype.get('register', function (req: any, res: any) {
+    ejs.renderFile('views/register.ejs', {}, function (err, data) {
         res.end(data);
     });
 });
 
-app.prototype.get('home', function(req: any, res: any) {
+app.prototype.get('home', function (req: any, res: any) {
     var data = '后台数据';
-    var list:Array<number> = [1, 2, 3, 4];
+    var list: Array<number> = [1, 2, 3, 4];
 
-    ejs.renderFile('views/index.ejs', {msg: data, l: list}, function(err, data) {
+    ejs.renderFile('views/index.ejs', { msg: data, l: list }, function (err, data) {
         res.end(data);
+    });
+});
+
+app.prototype.get('add', function (req: any, res: any) {
+    if (req.method == 'GET') {
+        console.log('get');
+        console.log(url.parse(req.url, true).query);
+    } else if (req.method == 'POST') {
+        var postStr = '';
+        req.on('data', function (chunk: any) {
+            postStr += chunk;
+        });
+        req.on('end', function (err: any, chunk: any) {
+            var param: any = {};
+            postStr.split("&").forEach(element => {
+                var key: string = element.split("=")[0];
+                var value = element.split("=")[1];
+                param[key] = value;
+            });
+
+            MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("test");
+                var myobj = { name: param['username'], age: param['age'], sex: param['sex'] };
+                dbo.collection("user").insertOne(myobj, function (err, result) {
+                    if (err) throw err;
+                    console.log("文档插入成功");
+                    res.end("<script>alert('文档插入成功'); history.back();</script>");
+
+                    // dbo.collection("user").find().toArray(function (err, result) {
+                    //     if (err) throw err;
+                    //     console.log(result);
+                    //     ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                    //         res.end(data);
+                    //     });
+                    //     db.close();
+                    // });
+
+                    db.close();
+                });
+            });
+            // res.end("<script>alert('创建成功');</script>"); history.back();
+        });
+    }
+});
+
+app.prototype.get('getuser', function (req: any, res: any) {
+    MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        dbo.collection("user").find().toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                res.end(data);
+            });
+            db.close();
+        });
+    });
+});
+
+app.prototype.get('del', function (req: any, res: any) {
+    var postStr = '';
+    req.on('data', function (chunk: any) {
+        postStr += chunk;
+    });
+    req.on('end', function (err: any, chunk: any) {
+        console.log(postStr);
+    });
+    res.end();
+    // MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+    //     if (err) throw err;
+    //     var dbo = db.db("test");
+    //     var whereStr = { "name": '菜鸟教程' };  // 查询条件
+    //     dbo.collection("site").deleteOne(whereStr, function (err, obj) {
+    //         if (err) throw err;
+    //         console.log("文档删除成功");
+    //         db.close();
+    //     });
+    // });
+});
+
+app.prototype.get('edit', function (req: any, res: any) {
+    MongoClient.connect(dburl, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        dbo.collection("user").find().toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            ejs.renderFile('views/userlist.ejs', { list: result }, function (err, data) {
+                res.end(data);
+            });
+            db.close();
+        });
     });
 });
 
@@ -78,7 +181,7 @@ app.prototype.get('home', function(req: any, res: any) {
 // // 装饰器
 // function get(params: any) {
 //     return function (target: any, methodName: any, desc: any) {
-        
+
 //     }
 // }
 
@@ -89,6 +192,6 @@ app.prototype.get('home', function(req: any, res: any) {
 
 //     @get('/login')
 //     getDat() {
-        
+
 //     }
 // }
